eb6bd9b1454a95a6b1d758520954c54568da89aa
dumbus <dumbusx@gmail.com>
2022-12-01 17:50:27 +0300

    refactor: refactor fs/copy to use async API

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { dirname, join } from 'node:path'
 import { readdir, mkdir, copyFile } from 'node:fs/promises';
-import { existsSync } from 'node:fs';
+import { access } from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,20 +10,25 @@ const copy = async () => {
     const pathToSource = join(__dirname, 'files');
     const pathToDestination = join(__dirname, 'files_copy');
 
-    const isSourceExists = existsSync(pathToSource);
-    const isDestinationExists = existsSync(pathToDestination);
-
     try {
-        if (!isSourceExists || isDestinationExists) {
-            throw new Error('FS operation failed');
-        } else {
-            const files = await readdir(pathToSource);
-            await mkdir(pathToDestination);
-    
-            for (const file of files) {
-                await copyFile(join(pathToSource, file), join(pathToDestination, file));
+        access(pathToDestination, async err => {
+            if (!err) {
+                throw new Error('FS operation failed');
             }
-        }
+        });
+
+        access(pathToSource, async err => {
+            if (err) {
+                throw new Error('FS operation failed');
+            } else {
+                const files = await readdir(pathToSource);
+                await mkdir(pathToDestination);
+        
+                for (const file of files) {
+                    await copyFile(join(pathToSource, file), join(pathToDestination, file));
+                }
+            }
+        });
     } catch (err) {
         console.log(err);
     }
