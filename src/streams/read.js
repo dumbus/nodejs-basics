1c8cfe1d6c46db1ebf278b28e6e0d39ba0cef469
dumbus <dumbusx@gmail.com>
2022-12-01 23:51:34 +0300

    feat: streams/read is done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToSource = join(__dirname, 'files', 'fileToRead.txt');
+
+    const readStream = createReadStream(pathToSource);
+    readStream.on('data', (chunk) => {
+        stdout.write(chunk + '\n');
+    });
+    
 };
 
 await read();
