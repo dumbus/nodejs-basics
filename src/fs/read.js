28310cb7e85ccb08c70e07624a43457eb341f529
dumbus <dumbusx@gmail.com>
2022-12-01 17:44:08 +0300

    feat: fs/read is done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import { readdir, readFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFolder = join(__dirname, 'files');
+    const fileName = 'fileToRead.txt';
+    const files = await readdir(pathToFolder);
+
+    try {
+        const isFileExists = files.includes(fileName);
+
+        if (isFileExists) {
+            const data = await readFile(join(pathToFolder, fileName), { encoding: 'utf8' });
+            console.log(data);
+        } else {
+            throw new Error('FS operation failed');
+        }
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await read();
