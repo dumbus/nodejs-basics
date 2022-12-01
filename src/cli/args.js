7036c41875c13d3e433ee638abf491c1ea8e2f56
dumbus <dumbusx@gmail.com>
2022-12-01 18:24:44 +0300

    feat: cli/args is done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const clArgs = argv.slice(2);
+    const result = [];
+
+    for (let i = 0; i < clArgs.length; i++) {
+        if (i % 2 === 0) {
+            clArgs[i] = clArgs[i].slice(2);
+        }
+
+        if (i % 2 !== 0) {
+            result.push(`${clArgs[Math.floor(i - 1)]} is ${clArgs[i]}`);
+        }
+    }
+    
+    console.log(result.join(', '));
 };
 
 parseArgs();
