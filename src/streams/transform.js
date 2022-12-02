5c5c3d34ba8a10e5f704d53a48c7b753b1872938
dumbus <dumbusx@gmail.com>
2022-12-02 23:07:45 +0300

    feat: streams/transform is done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform, pipeline } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+    const reversingStream = new Transform({
+        transform(chunk, enc, cb) {
+            const stringChunk = chunk.toString().trim();
+            const reversedChunk = stringChunk.split('').reverse().join('');
+
+            this.push(reversedChunk + '\n');
+
+            cb();
+        }
+    });
+
+    pipeline(
+        stdin,
+        reversingStream,
+        stdout,
+        error => {
+            console.log(error);
+        }
+    );
 };
 
 await transform();
