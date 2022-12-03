d2b216e40ea47ae9b4e28acf05107ffcad28b80f
dumbus <dumbusx@gmail.com>
2022-12-03 23:24:23 +0300

    feat: cp/ is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { spawn } from 'node:child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToChild = join(__dirname, 'files', 'script.js');
+
+    const child = spawn('node', [pathToChild, ...args]);
+    stdin.pipe(child.stdin);
+    child.stdout.pipe(stdout);
 };
 
-spawnChildProcess();
+// Функция, как и в условии, принимает масив аргументов args
+spawnChildProcess([1, 2, 3]);
